8f024a6cd820b36c49b2c0a674f33e8aff49d30a
dalseolhwa <seolhwa7898@gmail.com>
2022-05-30 17:05:29 +0900

    main.css 2치 main.js 2차 업로드

--- js/main.js
+++ js/main.js
@@ -12,6 +12,7 @@ $('.banner_slide .controls .prev').click(function(e){
     $('.slide_container').slick('slickPrev');
 });
 
+
 /*-------------------card_slide-------------------*/
 $('.card_tap_wrapper').slick({
     arrows:false,
@@ -29,12 +30,32 @@ $('.card_cont .controls .prev').click(function(e){
     $('.card_tap_wrapper').slick('slickPrev');
 });
 
+
 $('.card_slide .card_info_btn a:nth-child(2)').click(function(){
     $('.card_slide .controls').addClass('active');
 })
 $('.card_slide .card_info_btn a:nth-child(2)').siblings().click(function(){
     $('.card_slide .controls').removeClass('active');
 })
+
+let cBtn = $('.card_slide .card_info_btn a');
+let cList = $('.card_slide .card_cont');
+
+cBtn.click(function(e){
+    e.preventDefault();
+    cBtn.removeClass('current');
+    $(this).addClass('current');
+
+    cList.hide();
+
+    let targetClass = $(this).attr('data-filter');
+    let targetList = $(targetClass);
+
+    targetList.show();
+});
+
+
+
 
 /*-------------------benefit-------------------*/
 
