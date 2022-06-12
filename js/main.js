d8715d1582fecf7c06e843afe23a562a7639a33c
dalseolhwa <seolhwa7898@gmail.com>
2022-06-12 11:57:47 +0900

    2022-06-12

--- js/main.js
+++ js/main.js
@@ -10,16 +10,30 @@ $('.slide_container').slick({
 
 $('.banner_slide .controls .next').click(function(e){
     e.preventDefault();
-    $('.slide_container').slick('slickNext');
+    $('.banner_slide .slide_container').slick('slickNext');
 });
 $('.banner_slide .controls .prev').click(function(e){
     e.preventDefault();
-    $('.slide_container').slick('slickPrev');
+    $('.banner_slide .slide_container').slick('slickPrev');
+});
+
+$('.banner_slide .slide_container').on('afterChange', function(event, slick, currentSlide, nextSlide){ 
+    $('.banner_slide .paging .currnet').text(currentSlide + 1);
+  });
+
+
+$('.banner_slide .btns').click(function(){
+    $('.banner_slide .btns').toggleClass('active');
+    if($('.banner_slide .btns').hasClass('active')){
+        $('.banner_slide .slide_container').slick('slickPause');
+    }else{
+        $('.banner_slide .slide_container').slick('slickPlay');
+    }
 });
 
 
 /*-------------------card_slide-------------------*/
-$('.card_tap_wrapper').slick({
+$('.cardList .card_tap_wrapper').slick({
     arrows:false,
     infinite: true,
     slidesToShow: 5,
@@ -30,19 +44,20 @@ $('.card_tap_wrapper').slick({
 
 $('.card_cont .controls .next').click(function(e){
     e.preventDefault();
-    $('.card_tap_wrapper').slick('slickNext');
+    $('.card_slide .card_tap_wrapper').slick('slickNext');
 });
 $('.card_cont .controls .prev').click(function(e){
     e.preventDefault();
-    $('.card_tap_wrapper').slick('slickPrev');
+    $('.card_slide .card_tap_wrapper').slick('slickPrev');
 });
 
 
-$('.card_slide .card_info_btn a:nth-child(2)').click(function(){
-    $('.card_slide .controls').addClass('active');
+
+$('.cardList .card_info_btn a:nth-child(2)').click(function(){
+    $('.cardList .card_slide .controls').addClass('active');
 })
-$('.card_slide .card_info_btn a:nth-child(2)').siblings().click(function(){
-    $('.card_slide .controls').removeClass('active');
+$('.cardList .card_info_btn a:nth-child(2)').siblings().click(function(){
+    $('.cardList .card_slide .controls').removeClass('active');
 })
 
 let cBtn = $('.card_slide .card_info_btn a');
@@ -68,11 +83,28 @@ cBtn.click(function(e){
 
   $('.benefit_slide .controls .next').click(function(e){
     e.preventDefault();
-    $('.slide_container').slick('slickNext');
+    $('.benefit_slide .slide_container').slick('slickNext');
 });
 $('.benefit_slide .controls .prev').click(function(e){
     e.preventDefault();
-    $('.slide_container').slick('slickPrev');
+    $('.benefit_slide .slide_container').slick('slickPrev');
 });
 
+$('.benefit_slide .slide_container').on('afterChange', function(event, slick, currentSlide, nextSlide){
+    $('.benefit_slide .paging .currnet').text(currentSlide + 1);
+  });
 
+  $('.benefit_slide .paging .btns').click(function(){
+    $('.benefit_slide .paging .btns').toggleClass('active');
+    if($('.benefit_slide .paging .btns').hasClass('active')){
+        $('.benefit_slide .slide_container').slick('slickPause');
+    }else{
+        $('.benefit_slide .slide_container').slick('slickPlay');
+    }
+});
+
+/*-------------------ss_story-------------------*/
+
+$('.ss_story .slide_container').on('afterChange', function(event, slick, currentSlide, nextSlide){
+    $('.ss_story .paging .currnet').text(currentSlide + 1);
+  });
