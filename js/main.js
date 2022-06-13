9da704415faca572de5b072cd8c56858739ffca0
dalseolhwa <seolhwa7898@gmail.com>
2022-06-13 18:10:27 +0900

    2022-06-13

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
-/*-------------------mainCon banner-------------------*/
+/*-------------------Header-------------------*/
+
+$('header .search').click(function(){
+    $('.search_section').addClass('active');
+    $('body').css({overflow:'hidden'});
+});
+$('.close_btn').click(function(){
+    $('.search_section').removeClass('active');
+    $('body').css({overflow:'auto'});
+});
 
 
 /*-------------------mainCon banner-------------------*/
