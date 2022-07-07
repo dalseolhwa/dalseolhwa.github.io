9ef93989e78bc8d1b6131ae124e0628a81b6852f
dalseolhwa <seolhwa7898@gmail.com>
2022-07-07 18:09:30 +0900

    2022-07-07 반응형 전 최종예상

--- js/benefit.js
+++ js/benefit.js
@@ -8,3 +8,24 @@ $('.close_btn').click(function(){
     $('.search_section').removeClass('active');
     $('body').css({overflow:'auto'});
 });
+
+/*-------------------gamezone-------------------*/
+
+/*
+var image = $('img').httr('src'); //값을 반환, image변수에 저장
+
+        대상.attr('src','image.jpg');  //값을 변경
+
+*/ 
+
+let img = $('.qr_wrap img'),
+    imgSrc = img.attr('src');
+
+img.click(function(){
+    if($(this).attr('src') == 'images/img_qr.png'){
+        $(this).attr('src','images/img_qr 1@2x.png')
+    }else{
+        $(this).attr('src','images/img_qr.png')
+    }
+    
+});
