6d7882865122caaa3adea0f0e62f2c74ed9f23a7
dalseolhwa <seolhwa7898@gmail.com>
2022-07-08 11:41:21 +0900

    메인메뉴 오류 수정

--- js/customer.js
+++ js/customer.js
@@ -9,6 +9,21 @@ $('.close_btn').click(function(){
     $('body').css({overflow:'auto'});
 });
 
+let mainMenu = $('nav > ul > li'),
+    header = $('header'),
+    headerHeight = header.outerHeight();
+
+    mainMenu.each(function(){
+    $(this).mouseover(function(){
+        let subMenuHeight = $(this).find('ul').outerHeight();
+		let totalHeight = subMenuHeight + headerHeight;
+        header.css({height:`${totalHeight}px`});
+    });
+    $(this).mouseout(function(){
+        header.css({height:`${headerHeight}px`});
+    });
+    });
+
 /*-------------------QNA-------------------*/
 let keywordBtn = $('.qna .search_list a'),
     input = $('.qna .search_input input');
